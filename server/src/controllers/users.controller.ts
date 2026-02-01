69aad45430487bae6e59a425e07324d4c706c18e
Erick Neres <erickppn@gmail.com>
2026-02-01 16:19:16 -0300

    Add user discovery and follow/unfollow functionality

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -1,5 +1,6 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { getAuth } from "@clerk/fastify";
+import z from "zod";
 
 import { updateUserInputSchema } from "../schemas/user.schemas";
 
@@ -32,12 +33,12 @@ export async function getUserData(request: FastifyRequest, reply: FastifyReply) 
 export async function updateUserData(request: FastifyRequest, reply: FastifyReply) {
   try {
     const parsedFields = await parseMultipart(
-      request, 
+      request,
       fileUploader
     );
 
     const { fields, files } = updateUserInputSchema.parse(parsedFields);
-    const { username: inputUsername , bio, location, full_name } = fields;
+    const { username: inputUsername, bio, location, full_name } = fields;
 
     const { userId } = getAuth(request);
 
@@ -77,7 +78,7 @@ export async function updateUserData(request: FastifyRequest, reply: FastifyRepl
           { format: 'webp' },
           { width: '512' }
         ]
-      });   
+      });
 
       newUserData.profile_picture = url;
     }
@@ -102,5 +103,140 @@ export async function updateUserData(request: FastifyRequest, reply: FastifyRepl
     console.log(error);
 
     reply.status(401).send({ success: false, message: error });
+  }
+}
+
+// Find users by username, email, location, name
+export async function discoverUsers(request: FastifyRequest, reply: FastifyReply) {
+  const searchQuerySchema = z.object({
+    search_query: z.string()
+  });
+
+  try {
+    const { userId } = getAuth(request);
+    const { search_query } = searchQuerySchema.parse(request.query);
+
+    const allUsers = await User.find({
+      $or: [
+        { username: new RegExp(search_query, 'i') },
+        { email: new RegExp(search_query, 'i') },
+        { full_name: new RegExp(search_query, 'i') },
+        { location: new RegExp(search_query, 'i') },
+      ]
+    });
+
+    const filteredUsers = allUsers.filter(user => user._id !== userId);
+
+    reply.status(200).send({
+      success: true,
+      users: filteredUsers
+    });
+  } catch (error) {
+    console.log(error);
+
+    reply.status(401).send({ success: false, message: error });
+  }
+}
+
+// Follow User
+export async function followUser(request: FastifyRequest, reply: FastifyReply) {
+  const followUserParamsSchema = z.object({
+    id: z.string()
+  });
+
+  try {
+    const { userId } = getAuth(request);
+    const { id } = followUserParamsSchema.parse(request.params);
+
+    const loggedUser = await User.findById(userId);
+
+    if (!loggedUser) {
+      return reply.status(404).send({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    if (loggedUser?.following.includes(id)) {
+      return reply.status(400).send({
+        success: false,
+        message: "You are already follwing this user"
+      });
+    }
+
+    loggedUser.following.push(id);
+    await loggedUser.save();
+
+    const toUser = await User.findById(id);
+
+    if (!toUser) {
+      return reply.status(404).send({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    toUser.followers.push(loggedUser._id);
+    await toUser.save();
+
+    reply.status(200).send({
+      success: false,
+      message: "Now you are following this user"
+    })
+  } catch (error) {
+    console.log(error);
+
+    reply.status(401).send({
+      success: false,
+      message: error
+    });
+  }
+}
+
+// Unfollow User
+export async function unfollowUser(request: FastifyRequest, reply: FastifyReply) {
+  const followUserInputSchema = z.object({
+    id: z.string()
+  });
+
+  try {
+    const { userId } = getAuth(request);
+    const { id } = followUserInputSchema.parse(request.body);
+
+    const loggedUser = await User.findById(userId);
+
+    if (!loggedUser) {
+      return reply.status(404).send({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    loggedUser.following = loggedUser.following.filter(user => user !== userId);
+    await loggedUser.save();
+
+    const toUser = await User.findById(id);
+
+    if (!toUser) {
+      return reply.status(404).send({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    toUser.followers = toUser.followers.filter(user => user !== userId);
+    await toUser.save();
+
+    reply.status(200).send({
+      success: false,
+      message: "You are no longer following this user"
+    })
+  } catch (error) {
+    console.log(error);
+
+    reply.status(401).send({
+      success: false,
+      message: error
+    });
   }
 }
