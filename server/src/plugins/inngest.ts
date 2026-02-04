e43dbd63c831db47f2b791752492b73149605c44
Erick Neres <erickppn@gmail.com>
2026-02-04 00:29:22 -0300

    Refactor inngest plugin to use fastifyPlugin for improved registration

--- server/src/plugins/inngest.ts
+++ server/src/plugins/inngest.ts
@@ -1,16 +1,13 @@
 import fp from 'fastify-plugin';
-import { serve } from 'inngest/fastify';
+import { fastifyPlugin } from "inngest/fastify";
 
 import { inngest } from '@/jobs//client';
 import { functions } from '@/jobs/functions';
 
 export const inngestPlugin = fp(async app => {
-  app.route({
-    method: ["GET", "POST"],
-    url: '/api/inngest',
-    handler: serve({
-      client: inngest,
-      functions
-    }),
+  app.register(fastifyPlugin, {
+    client: inngest,
+    functions,
+    options: {},
   });
 });
